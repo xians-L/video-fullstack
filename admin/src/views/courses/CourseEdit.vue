73768b2b05e47cb3bf1a52a43ccb44c011c39065
xians <1163060734@qq.com>
2020-03-30 15:39:00 +0800

    element admin list

--- admin/src/views/courses/CourseEdit.vue
+++ admin/src/views/courses/CourseEdit.vue
@@ -2,7 +2,7 @@
   <div>
     <h4>{{ IsNew ? "创建" : "编辑" }}课程</h4>
     <ele-form
-      v-model="formData"
+      v-model="data"
       :form-desc="fields"
       :request-fn="submit"
     ></ele-form>
@@ -16,7 +16,6 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 export default class CourseEdit extends Vue {
   @Prop(String) id!: string;
   data = {};
-  formData = {};
 
   fields = {
     title: { label: "课程标题", type: "input" },
@@ -28,19 +27,21 @@ export default class CourseEdit extends Vue {
   }
 
   async submit(data: any) {
-    this.formData = {}
-    await this.$http.post("courses", data);
-    this.$message.success("创建成功");
+    let url = this.IsNew ? `courses` : `courses/${this.id}`
+    let methods = this.IsNew ? 'post' : 'put'
+    await this.$http[methods](url, data);
+    this.data = {}
+    this.$message.success("success");
     this.$router.go(-1)
   }
 
   async fetch() {
-    let res = await this.$http.get("courses");
+    let res = await this.$http.get(`courses/${this.id}`);
     this.data = res.data;
   }
 
   created() {
-    // this.fetch();
+    !this.IsNew && this.fetch();
   }
 }
 </script>
--- admin/src/views/courses/CourseList.vue
+++ admin/src/views/courses/CourseList.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+      <h4>视频列表</h4>
+      <el-button type="primary" @click="$router.push('/courses/creat')">添加视频</el-button>
+      
       <el-table
         :data="data.data"
         style="width: 100%">
@@ -9,8 +12,12 @@
           :label="field.label">
         </el-table-column>
         <el-table-column
-          :label="操作"
-          :width="200">
+          label="操作"
+          width="200">
+          <template slot-scope='scope'>
+            <el-button type="success" size="small" @click="$router.push(`/courses/edit/${scope.row._id}`)">编辑</el-button>
+            <el-button type="danger" size="small" @click="remove(scope.row)">删除</el-button>
+          </template>
         </el-table-column>
         
       </el-table>
@@ -27,6 +34,14 @@ export default class CourseList extends Vue {
     _id: {label: 'ID'},
     title: {label: '课程标题'},
     cover: {label: '课程封面图'}
+  }
+  async remove(row:any) {
+    try{
+     await this.$confirm('是否确认删除')
+    }catch(err){ return }
+    await this.$http.delete(`courses/${row._id}`)
+    this.$message.success('success')
+    this.fetch()
   }
 
   async fetch() {
